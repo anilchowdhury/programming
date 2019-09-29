aa4855137fef26b05f1d693d0c7f822378c6911f
Anil Chowdhury <41268154+anilchowdhury@users.noreply.github.com>
2019-09-29 22:50:33 +0530

    Add files via upload

---
+++ thread/interruptible/StoppableService.java
@@ -0,0 +1,36 @@
+package thread.interruptible;
+
+/**
+ * @author Anil Chowdhury
+ * Created on 9/29/2019
+ */
+public class StoppableService implements Runnable {
+
+    private String name;
+    private volatile boolean isStopped = false;
+
+    StoppableService(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        while (!isStopped) {
+            try {
+                System.out.println(String.format("%s is running ... ", name));
+                InterruptDriver.sleep();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(String.format("%s is STOPPED ... ", name));
+    }
+
+    synchronized void stop() {
+        isStopped = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
