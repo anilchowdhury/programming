6f3b09a88c21b282b9f7d412c5e1be0f1de4388d
Anil Chowdhury <41268154+anilchowdhury@users.noreply.github.com>
2019-11-02 13:33:52 +0530

    Add files via upload

---
+++ thread/water/leetcode/Hydrogen.java
@@ -0,0 +1,13 @@
+package thread.water.leetcode;
+
+/**
+ * @author Anil Chowdhury
+ * Created on 9/2/2019
+ */
+
+public class Hydrogen implements Runnable{
+    @Override
+    public void run() {
+        System.out.print("H ");
+    }
+}
