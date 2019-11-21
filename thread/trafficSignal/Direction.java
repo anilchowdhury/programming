60060f031bd93a45a551eae9afbcdafe5639f707
Anil Chowdhury <41268154+anilchowdhury@users.noreply.github.com>
2019-11-21 16:02:25 +0530

    Add files via upload

--- thread/trafficSignal/Direction.java
+++ thread/trafficSignal/Direction.java
@@ -1,9 +1,11 @@
 package thread.trafficSignal;
 
 /**
+ * Direction in which a vehicle can move after crossing the signal point
+ *
  * @author Anil Chowdhury
  * Created on 9/9/2019
  */
 public enum Direction {
-    LEFT, STRAIGHT, RIGHT;
+    LEFT, STRAIGHT, RIGHT
 }
