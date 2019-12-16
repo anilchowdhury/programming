4b6623dfc9d322f2fb426a610dfefaf8ad59e5e6
Anil Chowdhury <41268154+anilchowdhury@users.noreply.github.com>
2019-12-16 14:00:55 +0530

    Add files via upload

--- oldNotes/PairWithLessSum.java
+++ oldNotes/PairWithLessSum.java
@@ -3,6 +3,9 @@ package oldNotes;
 import java.util.Arrays;
 
 /**
+ * Given an array of integer & target sum, find how many pair{array[i], array[j]}
+ * can be formed such that sum of array[i] + array[j] < sum
+ *
  * @author Anil Chowdhury
  *         Created on 1/20/2019.
  */
@@ -10,7 +13,9 @@ public class PairWithLessSum {
 
     public static void main(String[] args){
         PairWithLessSum driver = new PairWithLessSum();
-        System.out.println(driver.getPairsCount(new int[]{5, 1, 12, 3, 6}, 10));
+        System.out.println(driver.getPairsCount(new int[]{5, 1, 12, 3, 6}, 10)); //5
+        System.out.println(driver.getPairsCount(new int[]{3, 1, 10, 11, 7, 9}, 7)); //1
+        System.out.println(driver.getPairsCount(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 7)); //6
 
     }
 
@@ -25,7 +30,7 @@ public class PairWithLessSum {
 
         Arrays.sort(array);
         while (left < right) {
-            if (array[left] + array[right] > sum) {
+            if (array[left] + array[right] >= sum) {
                 right--;
             } else {
                 pairCount += right - left;
