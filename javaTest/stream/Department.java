0c07caa8d1d380ab24af9d499e60b6243c0107cb
Anil Chowdhury <41268154+anilchowdhury@users.noreply.github.com>
2019-12-23 16:08:56 +0530

    Add files via upload

---
+++ javaTest/stream/Department.java
@@ -0,0 +1,18 @@
+package javaTest.stream;
+
+/**
+ * @author Anil Chowdhury
+ * Created on 12/4/2019
+ */
+class Department {
+
+    private String name;
+
+    Department(String name) {
+        this.name = name;
+    }
+
+    String getDepartmentName() {
+        return name;
+    }
+}
