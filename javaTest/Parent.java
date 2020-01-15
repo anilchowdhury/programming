b4f069caf829840e40ecfc7fa61a2e7a45bcac75
Anil Chowdhury <41268154+anilchowdhury@users.noreply.github.com>
2020-01-15 15:01:46 +0530

    Add files via upload

---
+++ javaTest/Parent.java
@@ -0,0 +1,25 @@
+package javaTest;
+
+/**
+ * @author Anil Chowdhury
+ *         Created on 2/27/2019.
+ */
+class Parent {
+    private static Parent singleton;
+
+    Parent() throws Exception {
+        if (getClass().getName().equals("Child")) {
+            throw new Exception();
+        }
+    }
+
+    private Parent(int i) {
+    }
+
+    public static Parent instance() throws Exception {
+        if (singleton == null) {
+            singleton = new Parent(1);
+        }
+        return singleton;
+    }
+}
