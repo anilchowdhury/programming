175a75b480a482faef642633ba2a0dcce3a578dd
Anil Chowdhury <41268154+anilchowdhury@users.noreply.github.com>
2020-01-28 15:20:10 +0530

    Add files via upload

--- javaTest/TemperatureSensor.java
+++ javaTest/TemperatureSensor.java
@@ -12,10 +12,35 @@ public class TemperatureSensor {
             public void wakeup() {
                 check();
             }
+
+            @Override
+            public Child overRideTest(Parent parentInstance) {
+                return null;
+            }
+
+            @Override
+            public Child overRideTest1(Parent parentInstance) {
+                return null;
+            }
+
+            /* This does not work. We can not pass as child instance as argument
+               as it will not override
+            @Override
+            public Child overRideTest1(Child parentInstance) {
+                return null;
+            }*/
         };
     }
 
     private boolean check() {
         return false;
+    }
+
+    public Parent calcualte(Parent instance) {
+        return null;
+    }
+
+    public Child calcualte(Child instance) {
+        return null;
     }
 }
